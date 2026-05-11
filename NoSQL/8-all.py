dacc97f0ef49a09648a6d127e9a8db473b90cd75
Your Name <you@example.com>
2026-05-11 12:00:15 +0400

    m

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -5,5 +5,5 @@ def list_all(mongo_collection):
     """Returns all documents in a collection or empty list"""
     if mongo_collection is None:
         return []
-
+    #  find() finds everything specific to mango_collection and list() list it as a python object.
     return list(mongo_collection.find())
