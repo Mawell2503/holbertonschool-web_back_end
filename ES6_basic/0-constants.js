19255397efc3d640ce390a0fcb93fb801067f575
Your Name <you@example.com>
2026-05-28 11:58:07 +0400

    m

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,14 +1,20 @@
+// export is keyword used to import function
 export function taskFirst() {
+  // const = constant
+  // it declares a variable and locks its value
+  // (doesnt allow for changes afterwards)
   const task = 'I prefer const when I can.';
   return task;
 }
-
+// this function returns the string 'is okay' 
 export function getLast() {
   return ' is okay';
 }
 
 export function taskNext() {
+  // let declares a variable.
   let combination = 'But sometimes let';
+  // combination + wtv is in function getLast
   combination += getLast();
 
   return combination;
--- ES6_basic/README.md
+++ ES6_basic/README.md
@@ -1,1 +1,4 @@
 # JavaScript
+In JS variables are declared using let & const
+const stands for constant;" keep this variable constant throughout the code"
+let stands for "let this variable change"
